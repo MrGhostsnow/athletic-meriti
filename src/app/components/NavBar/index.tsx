f416642fc0e4f1b5184a824964278dc7df131159
MrGhostsnow <96500538+MrGhostsnow@users.noreply.github.com>
2024-07-18 15:47:45 -0300

    feat: NavBar

---
+++ src/app/components/NavBar/index.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { ContainerNavBar, Logo, SectionLinks, StyledLink } from "./styles";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../assets/logo.png";
+
+const NavBar: React.FC = () => {
+  return (
+    <ContainerNavBar>
+      <Logo>
+        <Image src={logo} alt="Logo" width={309} height={309} />{" "}
+      </Logo>
+      <SectionLinks>
+        <Link href="/" passHref>
+          <StyledLink>CONHEÇA O ATHLETIC MERITI</StyledLink>
+        </Link>
+        <Link href="/about" passHref>
+          <StyledLink>PROJETOS E COTAÇÕES</StyledLink>
+        </Link>
+        <Link href="/contact" passHref>
+          <StyledLink>CONTATO</StyledLink>
+        </Link>
+        <Link href="/blog" passHref>
+          <StyledLink>DOE</StyledLink>
+        </Link>
+      </SectionLinks>
+    </ContainerNavBar>
+  );
+};
+
+export default NavBar;
---
+++ src/app/components/NavBar/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const ContainerNavBar = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+    padding: 10px 20px;
+    width: 70%;
+    height: 280px;
+    border: 1px solid red;
+    margin-top: 20px;
+    z-index: 2;
+`;
+
+export const Logo = styled.div`
+    
+`;
+
+export const SectionLinks = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 25px;
+    margin-top: 60px;
+    width: 80%;
+    font-size: 24px;
+    color: black;
+;
+`;
+
+export const StyledLink = styled.p``;
