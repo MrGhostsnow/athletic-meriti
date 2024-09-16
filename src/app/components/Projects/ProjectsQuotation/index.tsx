f899c678b92810ff085fa724ceb4c1bbf824f06d
MrGhostsnow <96500538+MrGhostsnow@users.noreply.github.com>
2024-09-16 17:05:28 -0300

    feat: loading

--- src/app/components/Projects/ProjectsQuotation/index.tsx
+++ src/app/components/Projects/ProjectsQuotation/index.tsx
@@ -38,64 +38,69 @@ const apiUrl = "https://back-athletic.onrender.com";
 const ProjectsQuotation: React.FC = () => {
   const [projects, setProjects] = useState<Project[]>([]);
   const [quotation, setQuotation] = useState<Quotation[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get(`${apiUrl}/projetos`)
-      .then((response) => {
-        setProjects(response.data);
-      })
-      .catch((error) => {
-        console.error("There was an error fetching the projects!", error);
-      });
-  }, []);
+    const fetchData = async () => {
+      try {
+        const [projectsResponse, quotationsResponse] = await Promise.all([
+          axios.get(`${apiUrl}/projetos`),
+          axios.get(`${apiUrl}/quotacoes`),
+        ]);
 
-  useEffect(() => {
-    axios
-      .get(`${apiUrl}/quotacoes`)
-      .then((response) => {
-        setQuotation(response.data);
-      })
-      .catch((error) => {
-        console.error("There was an error fetching the projects!", error);
-      });
+        setProjects(projectsResponse.data);
+        setQuotation(quotationsResponse.data);
+      } catch (error) {
+        console.error("There was an error fetching the data!", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
     <ContainerProjectQuotation>
       <TitleProjectQuotation>PROJETOS E COTAÇÕES</TitleProjectQuotation>
-      {projects.map((project) => (
-        <ProjectCard
-          proponente={project.proponente}
-          type={project.type}
-          imagem={project.imagem}
-          key={project._id}
-          fonteDeRecursos={project.fonteDeRecursos}
-          valorDoProjeto={project.valorDoProjeto}
-          status={project.status}
-          inicio={project.inicio}
-          fim={project.fim}
-          local={project.local}
-          numeroDeBeneficiarios={project.numeroDeBeneficiarios}
-          modalidadeEManifestacao={project.modalidadeEManifestacao}
-          empresasParceiras={project.empresasParceiras}
-          descricao={project.descricao}
-        />
-      ))}
+      {loading ? (
+        <p>Carregando...</p>
+      ) : (
+        <>
+          {projects.map((project) => (
+            <ProjectCard
+              proponente={project.proponente}
+              type={project.type}
+              imagem={project.imagem}
+              key={project._id}
+              fonteDeRecursos={project.fonteDeRecursos}
+              valorDoProjeto={project.valorDoProjeto}
+              status={project.status}
+              inicio={project.inicio}
+              fim={project.fim}
+              local={project.local}
+              numeroDeBeneficiarios={project.numeroDeBeneficiarios}
+              modalidadeEManifestacao={project.modalidadeEManifestacao}
+              empresasParceiras={project.empresasParceiras}
+              descricao={project.descricao}
+            />
+          ))}
 
-      {quotation.map((quotation) => (
-        <ProjectCard
-          proponente={quotation.proponente}
-          type={quotation.type}
-          imagem={quotation.imagem}
-          key={quotation._id}
-          valorDoProjeto={quotation.valorDoProjeto}
-          status={quotation.status}
-          inicio={quotation.inicio}
-          fim={quotation.fim}
-          descricao={quotation.descricao}
-        />
-      ))}
+          {quotation.map((quotation) => (
+            <ProjectCard
+              proponente={quotation.proponente}
+              type={quotation.type}
+              imagem={quotation.imagem}
+              key={quotation._id}
+              valorDoProjeto={quotation.valorDoProjeto}
+              status={quotation.status}
+              inicio={quotation.inicio}
+              fim={quotation.fim}
+              descricao={quotation.descricao}
+            />
+          ))}
+        </>
+      )}
     </ContainerProjectQuotation>
   );
 };
