447d54b29f6f69d474e650adfd14988713b34358
MrGhostsnow <96500538+MrGhostsnow@users.noreply.github.com>
2024-09-16 11:10:54 -0300

    fix: stylus

--- src/app/components/Home/Donate/styles.ts
+++ src/app/components/Home/Donate/styles.ts
@@ -4,9 +4,10 @@ import bgdonate from "../../../assets/bg-donate.png";
 
 export const ContainerDonate = styled.div`
     width: 100%;
-    height: 100vh;
+    height: 80vh;
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
     background-image: url(${bgdonate.src}); 
     background-size: cover;
